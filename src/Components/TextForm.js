284206a6141c2778bb6c8e4cf10200be42e972e1
balkar1998 <balkar48812@gmail.com>
2023-07-23 00:10:11 +0530

    add convert lower case button

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -9,18 +9,32 @@ const TextForm = (props) => {
         setText(newText)
     }
 
+    const handleDownClick = () => {
+        setText(text.toLowerCase())
+    }
+
     const handleOnChange = (e) => {
         setText(e.target.value)
     }
 
     return (
-        <div>
+        <>
+        <div className='container'>
             <h2>{props.header}</h2>
             <div className='mb-3'>
                 <textarea className='form-control' id='myBox' rows={8} value={text} onChange={handleOnChange} ></textarea>
             </div>
             <button className='btn btn-primary' onClick={handleUpClick} >Convert to upper case</button>
+            <button className='btn btn-primary mx-3' onClick={handleDownClick}>Conbert to lower case</button>
         </div>
+        <div className='container my-3'>
+            <h2>Your text summary</h2>
+            <p>{text.split(' ').length} word and {text.length} characters</p>
+            <p>{0.008 * text.split(' ').length} Minutes read</p>
+            <h3>Preview</h3>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
 
