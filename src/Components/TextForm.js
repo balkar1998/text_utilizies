d4b9f36f97cc027eb722d462a8e506e806e8a4ac
balkar1998 <balkar48812@gmail.com>
2023-07-23 02:35:14 +0530

    wordcount bug slved and few other button added

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 const TextForm = (props) => {
     
     const [text, setText] = useState('')
+    const [exWord, setExWord] = useState('')
+    const [style, setStyle] = useState({
+        background: 'white'
+    })
 
     const handleUpClick = () => {
         let newText = text.toUpperCase();
@@ -13,8 +17,51 @@ const TextForm = (props) => {
         setText(text.toLowerCase())
     }
 
+    const handleUndoClick = () => {
+        let undoWord = text.split(' ')
+        let word = undoWord.pop()
+        setExWord(word)
+        setText(undoWord.join(' '))
+    }
+
+    const handleRedoClick = () => {
+        let undoWord = text.split(' ')
+        undoWord.push(exWord)
+        console.log(exWord)
+        if(exWord != '')
+        {
+            setText(undoWord.join(' '))
+        }
+        setExWord('')
+    }
+
+    const handleClearClick = () => {
+        setText('')
+    }
+
     const handleOnChange = (e) => {
         setText(e.target.value)
+    }
+
+    const handleModeClick = () => {
+        if(style.background == 'white')
+        {
+            setStyle({background: 'red'})
+        }else{
+            setStyle({background: 'white'})
+        }
+    }
+    let wordCount = 0;
+    const countWord = (text) => {
+        let arr = text.split(' ')
+        console.log(arr)
+        for (let index = 0; index < arr.length; index++) {
+            if(arr[index] != '')
+            {
+                wordCount++;
+            }
+        }
+        return wordCount
     }
 
     return (
@@ -22,15 +69,19 @@ const TextForm = (props) => {
         <div className='container'>
             <h2>{props.header}</h2>
             <div className='mb-3'>
-                <textarea className='form-control' id='myBox' rows={8} value={text} onChange={handleOnChange} ></textarea>
+                <textarea className='form-control' id='myBox' rows={8} value={text} onChange={handleOnChange} style={style}></textarea>
             </div>
-            <button className='btn btn-primary' onClick={handleUpClick} >Convert to upper case</button>
-            <button className='btn btn-primary mx-3' onClick={handleDownClick}>Conbert to lower case</button>
+            <button className='btn btn-primary mx-3' onClick={handleUpClick} >Convert to upper case</button>
+            <button className='btn btn-primary mx-3' onClick={handleDownClick}>Convert to lower case</button>
+            <button className='btn btn-primary mx-3 my-4' onClick={handleUndoClick}>Undo by word</button>
+            <button className='btn btn-primary my-4' onClick={handleRedoClick}>Redo last word</button>
+            <button className='btn btn-primary mx-3 my-4' onClick={handleClearClick}>Clear text</button>
+            <button className='btn btn-primary mx-3' onClick={handleModeClick}>Change background color</button>
         </div>
         <div className='container my-3'>
             <h2>Your text summary</h2>
-            <p>{text.split(' ').length} word and {text.length} characters</p>
-            <p>{0.008 * text.split(' ').length} Minutes read</p>
+            <p>{countWord(text)} word and {text.length} characters</p>
+            <p>{0.008 * countWord(text)} Minutes read</p>
             <h3>Preview</h3>
             <p>{text}</p>
         </div>
